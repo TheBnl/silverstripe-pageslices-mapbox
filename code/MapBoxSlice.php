289fef8cc21ef8c7cc861d6ec5c45e3d7500538b
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2017-01-13 14:30:00 +0100

    the slice can now hold multiple markers/addresses

--- code/MapBoxSlice.php
+++ code/MapBoxSlice.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * UserFormSlice.php
+ * MapBoxSlice.php
  *
  * @author Bram de Leeuw
  * Date: 03/10/16
@@ -8,23 +8,60 @@
 
 
 /**
- * UserFormSlice
+ * MapBoxSlice
+ * @method HasManyList Addresses
  */
-class MapBoxSlice extends PageSlice
+class MapBoxSlice extends PageSlice implements UseMapBox
 {
     private static $db = array();
     private static $has_one = array();
+
+    private static $has_many = array(
+        'Addresses' => 'MapBoxSliceAddress'
+    );
+
     private static $slice_image = 'pageslices_mapbox/images/MapBoxSlice.png';
 
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
 
-        $addressFields = $fields->fieldByName('Root.Address')->Fields();
-        $fields->removeByName('Address');
-        $fields->addFieldsToTab('Root.Main', $addressFields);
+        $gridFieldConfig = new MapBoxSliceGridFieldConfig();
+        $gridField = new GridField('Addresses', 'Addresses', $this->Addresses(), $gridFieldConfig);
 
+        $fields->addFieldsToTab('Root.Main', array($gridField));
         return $fields;
+    }
+
+    /**
+     * Return a array of markers
+     *
+     * @return array
+     */
+    public function mapBoxMarkers()
+    {
+        if ($addresses = $this->Addresses()) {
+            $markers = array();
+            foreach ($this->Addresses() as $address) {
+                $markers[] = self::create_marker($address);
+            }
+            return $markers;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param MapBoxSliceAddress $address
+     * @return array
+     */
+    public static function create_marker(MapBoxSliceAddress $address) {
+        return array(
+            'ID' => $address->ID,
+            'Title' => $address->Title,
+            'Lat' => $address->Lat,
+            'Lng' => $address->Lng,
+        );
     }
 }
 
